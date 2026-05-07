a773903d1ae8bdd8153dece48844c33ce72ca9d8
marquesmaycon <mayconmarquesh@gmail.com>
2026-05-07 17:11:12 -0300

    styles fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
+          <main>{children}</main>
         </ThemeProvider>
       </body>
     </html>
