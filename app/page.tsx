ed53102c8615219d9e5bef53db53473c1a2b1aa5
marquesmaycon <mayconmarquesh@gmail.com>
2026-05-07 21:46:15 -0300

    tape draft

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/header"
 import { Contact } from "@/components/sections/contact";
 import { Hero } from "@/components/sections/hero"
+import { Tape } from "@/components/sections/tape";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Tape />
       <Contact />
     </>
   );
---
+++ components/sections/tape.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
+
+const items = [
+  "Performático",
+  "Acessível",
+  "Seguro",
+  "Interativo",
+  "Escalável",
+  "Intuitivo",
+  "Modular",
+  "Personalizável",
+  "Moderno",
+  "Rápido",
+  "Responsivo",
+  "Eficiente",
+  "Otimizado",
+  "Robusto",
+  "Flexível",
+];
+
+export function Tape() {
+  const container = useRef<HTMLDivElement>(null);
+
+  useGSAP(
+    () => {
+      const track = container.current;
+
+      if (!track) return;
+
+      const width = track.scrollWidth / 2;
+
+      const tween = gsap.to(track, {
+        x: -width,
+        duration: 16,
+        ease: "none",
+        repeat: -1,
+      });
+
+      track.addEventListener("mouseenter", () => {
+        gsap.to(tween, {
+          timeScale: 0.3,
+        });
+      });
+
+      track.addEventListener("mouseleave", () => {
+        gsap.to(tween, {
+          timeScale: 1,
+        });
+      });
+    },
+    { scope: container },
+  );
+
+  return (
+    <section className="overflow-hidden">
+      <div className="container">
+        <div className="flex mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div ref={container} className="divide flex flex-none divide-x pr-6">
+            {[...items, ...items].map((item, index) => (
+              <div
+                key={index}
+                className="flex h-20 items-center px-6 text-lg whitespace-nowrap"
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
