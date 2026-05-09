224b6492bbfce4deba254c9fe19b93de43bfb0ee
marquesmaycon <mayconmarquesh@gmail.com>
2026-05-09 15:24:15 -0300

    hero text animation

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -1,37 +1,69 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import { useRef } from "react";
+
+import { scrambleChars } from "@/lib/constans";
 
 import { SparklesExposion } from "../sparkles-explosion";
 import { Button } from "../ui/button";
 
+const words = ["produtos", "experiências", "soluções", "interfaces"];
+
 export function Hero() {
+  const textRef = useRef<HTMLSpanElement>(null);
+
+  useGSAP(() => {
+    const tl = gsap.timeline({
+      repeat: -1,
+    });
+
+    words.forEach((word) => {
+      tl.to(textRef.current, {
+        duration: 2,
+        scrambleText: {
+          text: word,
+          chars: scrambleChars,
+          speed: 0.4,
+        },
+      });
+
+      tl.to({}, { duration: 1.5 });
+    });
+  });
   return (
     <section
       id="home"
       className="relative flex h-screen items-center overflow-hidden"
     >
       <div className="container">
-        <div className="z-10 flex flex-col gap-16 md:text-center lg:gap-8">
+        <div className="z-10 flex flex-col gap-8 md:text-center lg:gap-16">
           <div>
-            <h3 className="font-heading text-md mb-6 font-medium uppercase lg:text-2xl">
-              Full-Stack Developer
+            <h3 className="font-heading text-md mb-6 font-medium uppercase lg:text-lg">
+              Disponível para novos projetos
             </h3>
             <h1 className="font-heading text-5xl font-bold lg:text-6xl lg:leading-20">
-              Building <span className="text-accent">amazing</span> <br />{" "}
-              Experiences
+              Construindo{" "}
+              <span className="text-accent" ref={textRef}>
+                {words[0]}
+              </span>{" "}
+              <br />
+              para a web.
             </h1>
           </div>
 
-          <div>
+          <div className="mx-auto text-balance lg:max-w-2/3">
             <p className="text-muted-foreground leading-7">
-              Meu nome é Maycon, sou um desenvolvedor de software focado em
-              experiências fluidas e impactantes.
+              Meu nome é Maycon, sou um desenvolvedor de software especializado
+              em construir interfaces modernas, APIs robustas e experiências web
+              de alta performance.
             </p>
           </div>
 
-          <div id="actions" className="space-x-4">
+          <div className="space-x-4">
             <Button size="xl" asChild className="font-heading">
               <Link href="#project">
                 <span className="scramble">Projetos</span> <ArrowRight />
@@ -41,7 +73,7 @@ export function Hero() {
               variant="outline"
               size="xl"
               asChild
-              className="font-heading"
+              className="font-heading bg-background/10 backdrop-blur-md"
             >
               <Link href="#contact">
                 <span className="scramble">Contato</span>
